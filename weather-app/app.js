58bd8f57162b0b92d0b58820b380ed6015e438bb
bibhutirajbhandari <bibhutirajbhandari@gmail.com>
2023-03-30 16:31:54 +0530

    updated file

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,25 +2,25 @@ const request = require('postman-request');
 
 //Request of weather through Latitude & Longitude
 
-// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=27.700769&lon=85.300140&appid=24586a321a4110eb8e15a1f249b602cb&units=metric’;
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=27.700769&lon=85.300140&appid=24586a321a4110eb8e15a1f249b602cb&units=metric’;
 
-// request({url: url, json: true },(error,response) => {
-   // const data = JSON.parse(response.body);
-   // console.log(data);
+request({url: url, json: true },(error,response) => {
+   const data = JSON.parse(response.body);
+   console.log(data);
 
-   // if(error) {
-   //    console.log("Unable to connect to weather service!");
-   // } else if(response.body.message) {
-   //    console.log("Unable to find weather location!");
-   // }else {
-   //    const temp = response.body.main.temp;
-   //    const feelsLike = response.body.main.feels_like;
-   //    const desc = response.body.weather[0].main;
+   if(error) {
+      console.log("Unable to connect to weather service!");
+   } else if(response.body.message) {
+      console.log("Unable to find weather location!");
+   }else {
+      const temp = response.body.main.temp;
+      const feelsLike = response.body.main.feels_like;
+      const desc = response.body.weather[0].main;
 
-   // console.log(`Weather: ${desc}`);
-   // console.log(`It's currently ${temp} .It feels like ${feelsLike}`);
-   // }
-   // })
+   console.log(`Weather: ${desc}`);
+   console.log(`It's currently ${temp} .It feels like ${feelsLike}`);
+   }
+   })
 
 //Request weather through city name
 
