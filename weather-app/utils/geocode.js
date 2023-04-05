896ce37337a2ae45479abd2c275faec530c05444
bibhutirajbhandari <bibhutirajbhandari@gmail.com>
2023-04-05 17:38:26 +0530

    small updates

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,30 +1,30 @@
 const request =require('postman-request');
 
 //using request
-// const geocode = (city,callback) => {
+const geocode = (city,callback) => {
 
-//     const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=24586a321a4110eb8e15a1f249b602cb&units=metric`;
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=24586a321a4110eb8e15a1f249b602cb&units=metric`;
  
-//     request ({url, json: true},(error,{body}) => {
-//        if(error) {
-//           callback('Unable to connect to weather services',undefined);
-//        }
-//        else if(body.message) {
-//           callback('Unable to connect find weather lcoation',undefined);
-//        }
-//        else {
-//           callback(undefined, {
-//              longitude: body.coord.lon,
-//              latitude: body.coord.lat,
-//              city: body.name,
-//           })
-//        }
-//     })
-//  }
+    request ({url, json: true},(error,{body}) => {
+       if(error) {
+          callback('Unable to connect to weather services',undefined);
+       }
+       else if(body.message) {
+          callback('Unable to connect find weather lcoation',undefined);
+       }
+       else {
+          callback(undefined, {
+             longitude: body.coord.lon,
+             latitude: body.coord.lat,
+             city: body.name,
+          })
+       }
+    })
+ }
 
  //from FETCH API
  let geocode = (city,callback) => {
-   // const city = ")879";
+
    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=24586a321a4110eb8e15a1f249b602cb&units=metric`;
 
    fetch(url)
@@ -38,13 +38,15 @@ const request =require('postman-request');
              city: data.name,
             })
       })
-      .catch(data => {
-         console.log(data);
-         callback(`Cod: ${data.cod}, ${data.message}`,undefined);
-      })
+      // .catch(data => {
+      //    console.log(data);
+      //    callback(`Cod: ${data.cod}, ${data.message}`,undefined);
+      // })
 
+      .catch(error => {
+         callback(`error message: ${error.message}`);
+      })
    })
 }
  
-
 module.exports = geocode;
